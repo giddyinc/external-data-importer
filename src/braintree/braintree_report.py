4ba5b8fe492c7d56efdc19c258694e40ed610a4c
minal@boxed <minal@boxed.com>
2019-08-05 16:03:59 -0700

    fix missing column data

--- src/braintree/braintree_report.py
+++ src/braintree/braintree_report.py
@@ -28,13 +28,14 @@ def get_braintree_connection(braintree_config):
     return braintree_connection
 
 def get_search_results(braintree_connection):
-    N = 1
+    N = 2
     date_N_days_ago = datetime.now() - timedelta(days=N)
+    yesterday = datetime.now() - timedelta(days=1)
 
     search_results = braintree_connection.transaction.search(
       braintree.TransactionSearch.settled_at.between(
         date_N_days_ago,
-        datetime.now()
+        yesterday
       ),
       (braintree.TransactionSearch.status == braintree.Transaction.Status.Settled)
     )
@@ -43,9 +44,10 @@ def get_search_results(braintree_connection):
 def copy_into_redshift(config, s3_key):
     db_config = config['braintree']['database']
     with psycopg2.connect(dbname=db_config["database"], user=db_config["user"], host=db_config["host"],port=db_config["port"],password=db_config["password"] ) as rs_conn:
-        LOG.info("redshift connected") 
+        LOG.info("redshift connected to host %s with user %s" % (db_config["host"],db_config["user"]))
+        LOG.info("copying to %s.%s" % (config['schema']['schema_name'],config['schema']['table_name']))
         with rs_conn.cursor() as rs_cur:
-            query_template = "COPY %s.%s (transaction_id,transaction_type,transaction_status,created_datetime,submitted_for_settlement_date,settlement_date,disbursement_date,merchant_account,amount_authorized,amount_submitted_for_settlement,service_fee,tax_amount,tax_exempt,purchase_order_number,order_gid,order_id,refunded_transaction_id,payment_instrument_type,card_type,customer_id,payment_method_token,customer_company,channel, processor,raw_data)    FROM 's3://%s/%s' credentials 'aws_access_key_id=%s;aws_secret_access_key=%s' CSV DELIMITER ',' TIMEFORMAT 'YYYY-MM-DD HH:MI:SS' IGNOREBLANKLINES IGNOREHEADER 1 TRUNCATECOLUMNS GZIP maxerror 10"
+            query_template = "COPY %s.%s (transaction_id,transaction_type,transaction_status,created_datetime,submitted_for_settlement_date,settlement_date,disbursement_date,merchant_account,amount_authorized,amount_submitted_for_settlement,service_fee,tax_amount,tax_exempt,purchase_order_number,order_id,refunded_transaction_id,payment_instrument_type,card_type,customer_id,payment_method_token,customer_company,channel, processor,raw_data)    FROM 's3://%s/%s' credentials 'aws_access_key_id=%s;aws_secret_access_key=%s' CSV DELIMITER ',' TIMEFORMAT 'YYYY-MM-DD HH:MI:SS' IGNOREBLANKLINES IGNOREHEADER 1 TRUNCATECOLUMNS GZIP maxerror 10"
 
             query = query_template % (
                     config['schema']['schema_name'],
@@ -117,18 +119,19 @@ def save_to_database(search_results,db_config,schema_name,table_name):
 
     return count_saved
 
-def process_search_results(search_results):
+def process_search_results(search_results,braintree_connection):
     search_results_processed = []
-    try:
-        for transaction in search_results.items:
-            t = {}
+    count = 0
+    for transaction in search_results.items:
+        t = {}
+        try:
             t['transaction_id'] = transaction.id
             t['transaction_type'] = transaction.type
             t['transaction_status'] = transaction.status
             t['transaction_created_at'] = transaction.created_at
             t['submitted_for_settlement_date'] = None
             t['settlement_date'] = None
-            t['transaction_disbursement_date'] = None#""transaction.disbursement_date" #if transaction.disbursement_date is None else transaction.disbursement_date
+            t['transaction_disbursement_date'] = transaction.disbursement_details.disbursement_date
             t['transaction_merchant_account_id'] = transaction.merchant_account_id
             t['amount_authorized'] = None
             t['amount_submitted_for_settlement'] = None
@@ -136,31 +139,48 @@ def process_search_results(search_results):
             t['transaction_tax_amount'] = transaction.tax_amount
             t['transaction_tax_exempt'] = transaction.tax_exempt
             t['transaction_purchase_order_number'] = transaction.purchase_order_number
-            t['transaction_order_gid'] = "transaction.order_gid"
             t['transaction_order_id'] = transaction.order_id
             t['transaction_refunded_transaction_id'] = transaction.refunded_transaction_id
             t['transaction_payment_instrument_type'] = transaction.payment_instrument_type
             t['transaction_card_type'] = transaction.credit_card_details.card_type
-            t['transaction_customer_id'] = "transaction.customer_id"# or "transaction.customer_id"
+            t['transaction_customer_id'] = None
             t['transaction_token'] = transaction.credit_card_details.token
-            t['transaction_customer_company'] = "transaction.customer_company"
+            t['transaction_customer_company'] = None
             t['transaction_channel'] = transaction.channel
-            t['transaction_processor'] = "processor"
+            t['transaction_processor'] = None
             t['raw_data'] = str(transaction)
 
             for status_event in transaction.status_history:
                 if status_event.status == "submitted_for_settlement":
-                    submitted_for_settlement_date = status_event.timestamp
-                    amount_submitted_for_settlement = status_event.amount
+                    t['submitted_for_settlement_date'] = status_event.timestamp
+                    t['amount_submitted_for_settlement'] = status_event.amount
                 elif status_event.status == "settled":
-                    settlement_date = status_event.timestamp
+                    t['settlement_date'] = status_event.timestamp
                 elif status_event.status == "authorized":
-                    amount_authorized = status_event.amount
+                    t['amount_authorized'] = status_event.amount
+
+            if (t['transaction_payment_instrument_type'] == "paypal_account".lower()):
+                 t['transaction_processor'] = "Paypal"
+            elif (t['transaction_card_type'] == "Amex Express".lower() or t['transaction_card_type'] == "American Express".lower()):
+                 t['transaction_processor'] = "American Express Merchant Account"
+            elif (t['transaction_card_type'] == "Visa".lower() or t['transaction_card_type'] == "Mastercard".lower() or t['transaction_card_type'] == "Discover".lower()):
+                 t['transaction_processor'] = "Braintree"
+            elif (t['transaction_card_type'] == "UnionPay".lower() or t['transaction_card_type'] == "Elo".lower() or t['transaction_card_type'] == "JCB".lower()):
+                 t['transaction_processor'] = "Braintree"
+            else:
+                 t['transaction_processor'] = "ERROR - UPDATE LOGIC"
+
+            if (transaction.vault_customer is not None):
+                t['transaction_customer_id'] =  transaction.vault_customer.id
+                t['transaction_customer_company'] = transaction.vault_customer.company
 
             search_results_processed.append(t)
-    except Exception as e:
-        LOG.info(str(e))
-
+        except Exception as e:
+            LOG.error(str(e))
+            LOG.error("Error Processing transaction %s" % transaction)
+        count = count + 1
+        #if(count > 5):
+        #    break
     return search_results_processed
 
 def get_data():
@@ -180,7 +200,7 @@ def get_data():
     braintree_connection = get_braintree_connection(config['braintree']['braintree'])
     LOG.info("braintree_connection %s" % braintree_connection)
     search_results = get_search_results(braintree_connection)
-    LOG.info("search_results: %s " % search_results)
+    LOG.info("search_results count : %s " % search_results.maximum_size)
 
     #write to s3
     batchTimestamp = datetime.now()
@@ -192,17 +212,15 @@ def get_data():
     file_path = os.path.join(config.get('PG_DUMP_TEMP_DIR'), str(batchTimestamp.strftime('%Y-%m-%d_%H_%M_%S.%f'))) + '.gz'
     s3_key_name = os.path.join(config['S3']['UPLOAD_DIR'],batchTimestamp.strftime('%Y-%m-%d/%H_%M_%S.%f')) + '.gz'
 
-    search_results_processed = process_search_results(search_results)
-    LOG.info("search_results processed")
+    search_results_processed = process_search_results(search_results,braintree_connection)
+    LOG.info("count of search_results processed %s " % len(search_results_processed))
 
     with gzip.open( file_path , 'wt') as tempfile:
         csv_writer = csv.writer(tempfile, dialect=format)#, fieldnames=fieldnames )
-        csv_writer.writerow(['transaction_id','transaction_type','transaction_status','created_datetime','submitted_for_settlement_date','settlement_date,disbursement_date','merchant_account','amount_authorized,amount_submitted_for_settlement,service_fee','tax_amount','tax_exempt','purchase_order_number','order_gid','order_id','refunded_transaction_id','payment_instrument_type','card_type','customer_id','payment_method_token','customer_company','channel','processor','raw_data'])
+        csv_writer.writerow(['transaction_id','transaction_type','transaction_status','created_datetime','submitted_for_settlement_date','settlement_date,disbursement_date','merchant_account','amount_authorized,amount_submitted_for_settlement,service_fee','tax_amount','tax_exempt','purchase_order_number','order_id','refunded_transaction_id','payment_instrument_type','card_type','customer_id','payment_method_token','customer_company','channel','processor','raw_data'])
         for r in search_results_processed:
-            csv_writer.writerow([r['transaction_id'],r['transaction_type'],r['transaction_status'],r['transaction_created_at'],r['submitted_for_settlement_date'],r['settlement_date'],r['transaction_disbursement_date'],r['transaction_merchant_account_id'],r['amount_authorized'],r['amount_submitted_for_settlement'],r['transaction_service_fee_amount'],r['transaction_tax_amount'],r['transaction_tax_exempt'],r['transaction_purchase_order_number'],r['transaction_order_gid'],r['transaction_order_id'],r['transaction_refunded_transaction_id'],r['transaction_payment_instrument_type'],r['transaction_card_type'],r['transaction_customer_id'],r['transaction_token'],r['transaction_customer_company'],r['transaction_channel'],r['transaction_processor'],r['raw_data']])
+            csv_writer.writerow([r['transaction_id'],r['transaction_type'],r['transaction_status'],r['transaction_created_at'],r['submitted_for_settlement_date'],r['settlement_date'],r['transaction_disbursement_date'],r['transaction_merchant_account_id'],r['amount_authorized'],r['amount_submitted_for_settlement'],r['transaction_service_fee_amount'],r['transaction_tax_amount'],r['transaction_tax_exempt'],r['transaction_purchase_order_number'],r['transaction_order_id'],r['transaction_refunded_transaction_id'],r['transaction_payment_instrument_type'],r['transaction_card_type'],r['transaction_customer_id'],r['transaction_token'],r['transaction_customer_company'],r['transaction_channel'],r['transaction_processor'],r['raw_data']])
     LOG.info("Wrote to  temp file %s" % file_path)
-
-                    
 
     s3conn = utils.s3_connect(config['AWS']['ACCESSKEY'], config['AWS']['SECRETKEY'])
     utils.s3_upload(s3conn, config['S3']['BUCKET'], s3_key_name, file_path)
@@ -210,9 +228,6 @@ def get_data():
     os.remove(file_path)
 
     copy_into_redshift(config, s3_key_name)
-
-    #count_saved = save_to_database(search_results,config['braintree']['database'],config['schema']['schema_name'],config['schema']['table_name'])
-    #LOG.info("Script Complete - saved %s rows to database" % count_saved)
 
 if __name__=="__main__":
     get_data()
