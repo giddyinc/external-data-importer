3ae321b1e8dbd0af5d4ddbaeb75e5dab2a7a84aa
minal@boxed <minal@boxed.com>
2019-11-26 15:33:58 -0800

    process ups file

---
+++ src/ups/process_file.py
@@ -0,0 +1,95 @@
+#csv reader reads the file line by line
+#first line matches required columns to file columns and makes an order
+#if any columns are missing throw an error
+
+#create table for UPS types
+
+#File = ubd_spk_wky_boxed -- UBD_SPK_WKY_BOXED_20191006_20191012_UBD.CSV
+# table =  external_sources.ups_invoices
+
+import os
+import sys
+import logging
+import pandas as pd
+import re
+import s3fs
+from datetime import datetime
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, \
+    format='%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s', \
+    datefmt="%Y-%m-%d %H:%M:%S", \
+    stream=sys.stdout)
+
+def make_snake_case(x):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', x)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+from functools import reduce
+
+def change_case(str): 
+    return reduce(lambda x, y: x + ('_' if y.isupper() else '') + y, str).lower() 
+
+required_cols = [
+                'payer_account',
+                'invoice_date',
+                'invoice_num',
+                'invoice_amt',
+                'transaction_date',
+                'ship_ref_num1',
+                'ship_ref_num2',
+                'bill_option_code',
+                'pkg_quantity',
+                'tracking_num',
+                'entered_weight',
+                'billed_weight',
+                'container_type',
+                'pkg_dimension',
+                'zone',
+                'charge_category_code',
+                'charge_category_detail_code',
+                'charge_class_code',
+                'charge_desc_code',
+                'charge_desc',
+                'incentive_amt',
+                'net_amt',
+                'sender_name',
+                'sender_company',
+                'sender_add1',
+                'sender_add2',
+                'sender_city',
+                'sender_state',
+                'sender_postal',
+                'sender_country_or_territory',
+                'receiver_name',
+                'receiver_company',
+                'receiver_add1',
+                'receiver_add2',
+                'receiver_city',
+                'receiver_state',
+                'receiver_postal',
+                'receiver_country_or_territory',
+                ]
+
+
+def process_file(local_path,file,s3_path):
+    df = pd.read_csv(local_path+file, low_memory=False)
+    current_columns = df.columns.tolist()
+    new_columns = [make_snake_case(i)  for i in current_columns]
+    df.columns = new_columns
+
+    #drop extra header row
+    df = df.drop(0)
+    df = df[required_cols]
+
+    desc_csv = pd.read_csv('s3://boxed-pensieve-s3-redshift/ups/test/ups_conversion/Charge_Descriptions_Key.csv')
+    final_df = df.merge(desc_csv,on='charge_desc')
+    LOG.info("merged new file")
+
+    final_df['date_uploaded_at'] = datetime.now()
+    final_df.to_csv(s3_path+file, index=False)
+    LOG.info("uploaded new file to S3")
+
+    #upload file to S3
+    # copy to redshift
+
